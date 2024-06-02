37c33733575d9fa9141ba6651b73b1de36c5047f
SPHLam <samphanhoailam@gmail.com>
2024-06-03 01:58:51 +0700

    Implement package pickup and deliver to customer, Boost and Bumps

--- Assets/Scripts/Driver.cs
+++ Assets/Scripts/Driver.cs
@@ -9,11 +9,21 @@ public class Driver : MonoBehaviour
     [SerializeField]
     private float _moveSpeed = 20f;
 
+    private bool _hasPackage = false;
+    private bool _isSpeedBuff = false;
+
+    [SerializeField]
+    private Color _packageColor = new Color(1,1,0,1);
+
+    private SpriteRenderer _spriteRenderer;
+
+    private float _speedBuff = 2f;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        _spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
@@ -27,11 +37,41 @@ public class Driver : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        Debug.Log(collision.transform.name);
+        if (collision.transform.tag == "World objects")
+        {
+            _moveSpeed = 20f;
+            _isSpeedBuff = false;
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        Debug.Log(collision.transform.name);
+        if (collision.tag == "Package")
+        {
+            if (!_hasPackage)
+            {
+                _hasPackage = true;
+                SpriteRenderer package = collision.GetComponent<SpriteRenderer>();
+                _spriteRenderer.color = package.color;
+                Destroy(collision.gameObject);
+            }
+        }
+        else if (collision.tag == "Customer")
+        {
+            if (_hasPackage)
+            {
+                _hasPackage = false;
+                _spriteRenderer.color = _packageColor;
+                Debug.Log("Package delivered");
+            }
+        }
+        else if (collision.tag == "Speed pad")
+        {
+            if (!_isSpeedBuff)
+            {
+                _moveSpeed *= _speedBuff;
+                _isSpeedBuff = true;
+            }
+        }
     }
 }
